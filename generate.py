81c88fd973649a4ccef7e24c13f12262697d4e65
zach-vanzura <zvanzura@uvm.edu>
2025-02-01 12:43:41 -0500

    Completed Assignment 03, made 3 link 2 joint robo

--- generate.py
+++ generate.py
@@ -3,9 +3,6 @@ import pyrosim.pyrosim as pyrosim
 length, width, height = 1, 1, 1
 x, y, z = 0, 0, 0.5
 
-x_root, y_root, z_root = 0, 0, 0.5
-x_child, y_child, z_child = 0, 0, 0.5
-
 
 def create_world():
 
@@ -17,24 +14,11 @@ def create_world():
 def create_robot():
 
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Link0", pos=[x_root, y_root, z_root], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0, 0, 1])
-    pyrosim.Send_Cube(name="Link1", pos=[x_child, y_child, z_child], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[0, 0, 1])
-    pyrosim.Send_Cube(name="Link2", pos=[x_child, y_child, z_child], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link2_Link3", parent="Link2", child="Link3", type="revolute", position=[0, 0.5, 0.5])
-    pyrosim.Send_Cube(name="Link3", pos=[0, 0.5, 0], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link3_Link4", parent="Link3", child="Link4", type="revolute", position=[0, 1, 0])
-    pyrosim.Send_Cube(name="Link4", pos=[x_child, 0.5, 0], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link4_Link5", parent="Link4", child="Link5", type="revolute", position=[0, 0.5, -0.5])
-    pyrosim.Send_Cube(name="Link5", pos=[0, 0, -0.5], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link5_Link6", parent="Link5", child="Link6", type="revolute", position=[0, 0, -1])
-    pyrosim.Send_Cube(name="Link6", pos=[0, 0, -0.5], size=[length, width, height])
-
-
-
-
-
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[length, width, height])
 
     pyrosim.End()
 
