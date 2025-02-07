aa3b2c905c13f0a74a4382486d955c97cbccdc69
zach-vanzura <zvanzura@uvm.edu>
2025-02-07 10:28:33 -0500

    Added a front leg sensor and functionality to plot the data.

--- analyze.py
+++ analyze.py
@@ -1,9 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-backlegSensorValues = np.load('./data/backLegSensorValues.npy')
+backLegSensorValues = np.load('./data/backLegSensorValues.npy')
+frontLegSensorValues = np.load('./data/frontLegSensorValues.npy')
 
-print(backlegSensorValues)
+print(backLegSensorValues)
+print(frontLegSensorValues)
 
-plt.plot(backlegSensorValues)
+plt.plot(backLegSensorValues, linewidth=3, label='Back Leg')
+plt.plot(frontLegSensorValues, label='Front Leg')
+plt.legend()
 plt.show()
--- simulate.py
+++ simulate.py
@@ -8,22 +8,24 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)  # set to 0 to enable
 p.setGravity(0,0,-9.8)
-plane_id = p.loadURDF("plane.urdf")
-robot_id = p.loadURDF("body.urdf")
+planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
-pyrosim.Prepare_To_Simulate(robot_id)
+pyrosim.Prepare_To_Simulate(robotId)
 
 MAX_TIME = 100
 
 backLegSensorValues = np.zeros(MAX_TIME)
+frontLegSensorValues = np.zeros(MAX_TIME)
 
 for i in range(MAX_TIME):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1 / 60)
 
 p.disconnect()
 
 np.save("./data/backLegSensorValues.npy", backLegSensorValues)
+np.save("./data/frontLegSensorValues.npy", frontLegSensorValues)
 
-print(backLegSensorValues)
